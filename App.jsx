e89665b9bd152bb0e7a2ecf763c795ef6c496cd0
yjb23tech <yjb23tech@gmail.com>
2023-12-24 14:03:35 +0000

    Building the skeleton out

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client"
+
+const App = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(<App />)
+
