222a69e1de2da27ed9eb04eb7d2a8bcd574482ec
yjb23tech <yjb23tech@gmail.com>
2023-12-24 22:04:43 +0000

    Upgrade U

--- App.jsx
+++ App.jsx
@@ -15,6 +15,37 @@ const App = () => {
                 genre_icon="genre_icon.png"
                 genre_name="Sophisticated Ignorance"
             />
+            <Card
+                song_image="album_cover_6.png"
+                song_title="Gorgeous"
+                album_icon="album_icon.png"
+                album_name="My Beautiful Dark Twisted Fantasy"
+                artist_icon="artist_icon.png"
+                artist_name="Kanye West"
+                genre_icon="genre_icon.png"
+                genre_name="Sophisticated Ignorance"
+            />
+            <Card 
+                song_image="album_icon_7.png"
+                song_title="POWER REMIX"
+                album_icon="album_icon.png"
+                album_name="My Beautiful Dark Twisted Fantasy"
+                artist_icon="artist_icon.png"
+                artist_name="Kanye West"
+                genre_icon="genre_icon.png"
+                genre_name="Sophisticated Ignorance"
+            />
+            <Card 
+                song_image="album_cover_8.png"
+                song_title="Hell Of A Life"
+                album_icon="album_icon.png"
+                album_name="My Beautiful Dark Twisted Fantasy"
+                artist_icon="artist_icon.png"
+                artist_name="Kanye West"
+                genre_icon="genre_icon.png"
+                genre_name="Sophisticated Ignorance"
+            />
+
         </div>
     )
 }
